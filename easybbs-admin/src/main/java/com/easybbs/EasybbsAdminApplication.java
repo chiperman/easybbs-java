922e2df387211e7c3033acabffb70f122fbe9f1d
chiperman <chipermanju@gmail.com>
2023-08-17 21:14:33 +0800

    Init commit

---
+++ easybbs-admin/src/main/java/com/easybbs/EasybbsAdminApplication.java
@@ -0,0 +1,11 @@
+package com.easybbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasybbsAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EasybbsAdminApplication.class, args);
+    }
+}
---
+++ easybbs-web/src/main/java/com/easybbs/EasybbsWebApplication.java
@@ -0,0 +1,11 @@
+package com.easybbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasybbsWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EasybbsWebApplication.class, args);
+    }
+}
