93e4094f57c438d45687d966f0ca06b2b1012d3b
chiperman <chipermanju@gmail.com>
2023-08-22 09:20:43 +0800

    feat(email): 新增验证码、发邮件等接口内容 (#5)

--- easybbs-common/src/main/java/utils/SetResponseUtils.java
+++ easybbs-common/src/main/java/utils/SetResponseUtils.java
@@ -12,6 +12,11 @@ public class SetResponseUtils {
         setResponse(response, EHttpCode.FAIL, data);
     }
 
+    public static void setResponseFailParam(MyResponse response, Object data) {
+        setResponse(response, EHttpCode.FAIL_Params, data);
+    }
+
+
     private static void setResponse(MyResponse response, EHttpCode httpCode, Object data) {
         response.setStatus(httpCode.getStatus());
         response.setCode(httpCode.getCode());
